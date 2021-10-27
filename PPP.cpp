ebc64378d19e51fd9809b4332289038a2fe57c24
bs-code23 <90001972+bs-code23@users.noreply.github.com>
2021-10-26 23:07:50 -0500

    Update PPP.cpp
    this is the comp2710 solution
    id recommend doing it this way and just fix check and read file functions

--- PPP.cpp
+++ PPP.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <vector>
 #include <algorithm>
 using namespace std;
@@ -9,37 +11,26 @@ int algorithm2(vector<int> &X);
 int algorithm3(vector<int> &X, int L, int U);
 int algorithm4(vector<int> &X);
 
-// global variables
+// reading/writing/checking file functions
+bool check_file(string);
+int read_file(vector<int> &X, ifstream& inStream);  // i want this to read the file and also store the integers into the vector as well
+void writefile(vector<int> &X, int outputArray_size, string file);
 
 // main method
 int main() {
-    // this link is where this solution was from
-    // https://stackoverflow.com/questions/40945378/reading-a-comma-separated-file-into-an-integer-array
-    ifstream inputFile ("phw_input.txt");
-
-    vector<int> numbers;
-    string number_as_string;
-
-    while (getline(inputFile, number_as_string, ',')) {
-        numbers.push_back(stoi(number_as_string));
-    }
-
-    cout << "n : " << numbers.size() << "\n";
-    for (auto&& number : numbers) {
-        cout << number << "\n";
-    }
+    check_file("phw_input.txt");
 }
 
 // function/algorithm definitions
 // each algorithm is a solution to MSCS
 
-// #1
+// T(n) = 
 int algorithm1(vector<int> &X) {
     int maxSoFar = 0;
     int P = 0;
     int Q = X.size();
     for (int L = P; L < Q; L++) {
-        for (int U = L; U < Q; U++) {
+        for (int U = L; U < Q; U++) {	
             int sum = 0;
             for (int I = L; I <= U; I++) {
                 sum = sum + X[I]; 
@@ -113,4 +104,48 @@ int algorithm4(vector<int> &X) {
 		maxSoFar = max(maxSoFar, maxEndingHere);
 	}
 	return maxSoFar;
+}
+
+// helper functions
+
+bool check_file(string file) {
+    ifstream stream;
+    
+    stream.open(file.c_str());
+    if (stream.fail()) {
+        cout << "Input file opening failed." << endl;
+    }
+    stream.close();
+
+    return true;
+}
+
+int read_file(vector<int> &X, ifstream& inStream) {
+    int i = 0;
+    string number_as_string;
+    while (getline(inStream, number_as_string, ',')) {
+        X.push_back(stoi(number_as_string));
+    }
+    for (vector<int>::iterator it = X.begin(); it != X.end(); ++it) {
+        cout << ' ' << *it;
+        cout << '\n';
+    }
+    return i;
+}
+
+void writefile(vector<int> &X, int outputArray_size, string file) {
+	ofstream outstream;
+    string output_filename;
+    cout << "Enter the output file name: ";
+    cin >> output_filename;
+
+    outstream.open((char*)output_filename.c_str());
+    for (int i = 0; i < outputArray_size; i++) {
+        outstream << X[i] << "\n";
+    }
+    outstream.close();
+
+    cout << "*** Please check the new file - " 
+         << output_filename << " ***\n"
+         << "*** Goodbye. ***";
 }
